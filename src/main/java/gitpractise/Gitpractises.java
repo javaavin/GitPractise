f8fcc0f684d2da23c87e0b31cd5fba4f3d3b9ea6
javaavin <java.avin@gmail.com>
2023-09-24 16:29:39 +0530

    Completed task 101

--- src/main/java/gitpractise/Gitpractises.java
+++ src/main/java/gitpractise/Gitpractises.java
@@ -3,7 +3,12 @@ package gitpractise;
 public class Gitpractises {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Task 101
+		
+		int a=10; int b=20; int c=a+b;
+		
+		System.out.println(c);
+		System.out.println("Total of a and b");
 
 	}
 
